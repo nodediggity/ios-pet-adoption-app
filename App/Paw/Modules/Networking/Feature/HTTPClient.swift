696b1f534c3e804773a457a94dd0c84a1bea0b77
nodediggity <iamgordonsmith@icloud.com>
2022-08-30 11:49:46 +0100

    feat: define `HTTPClient` abstract interface

---
+++ App/Paw/Modules/Networking/Feature/HTTPClient.swift
@@ -0,0 +1,18 @@
+//
+// HTTPClient.swift
+//
+
+import Foundation
+
+public protocol HTTPClientTask {
+    func cancel()
+}
+
+public protocol HTTPClient {
+    typealias Result = Swift.Result<(Data, HTTPURLResponse), Error>
+
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible to dispatch to appropriate threads, if needed.
+    @discardableResult
+    func dispatch(_ request: URLRequest, completion: @escaping (Result) -> Void) -> HTTPClientTask
+}
