034b0b7bfeebdf68696aba1722dae8abd411f6fc
nodediggity <iamgordonsmith@icloud.com>
2022-09-01 10:15:50 +0100

    test: extend `HTTPURLResponse` to support creation with status code

---
+++ App/PawTests/TestHelpers/HTTPURLResponse+StatusCode.swift
@@ -0,0 +1,11 @@
+//
+// HTTPURLResponse+StatusCode.swift
+//
+
+import Foundation
+
+extension HTTPURLResponse {
+    convenience init(statusCode: Int) {
+        self.init(url: URL(string: "https://any-url.com")!, statusCode: statusCode, httpVersion: nil, headerFields: nil)!
+    }
+}
